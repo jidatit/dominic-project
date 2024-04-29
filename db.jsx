9075b9cb20cd2458c6292196cc3e8a5c4e67ede4
Mohammad Nehaal <108192533+Mnehaal2000@users.noreply.github.com>
2024-04-29 17:07:12 +0500

    initial setup

---
+++ db.jsx
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9EoYEW4q-CuhX3j-kz6pVwTwhJhxfyRk",
+  authDomain: "dominic-project-30d16.firebaseapp.com",
+  projectId: "dominic-project-30d16",
+  storageBucket: "dominic-project-30d16.appspot.com",
+  messagingSenderId: "907287468214",
+  appId: "1:907287468214:web:b60381281108cefc70ae17"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
+export const analytics = getAnalytics(app);
