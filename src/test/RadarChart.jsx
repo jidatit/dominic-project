2844e0ba26376754bbc5f2260c3d47602d8d3224
Zubair Zahid <zubairzahid228@gmail.com>
2024-07-23 16:31:51 +0500

    charts added
    static and dynamic , both charts added ,

--- src/test/RadarChart.jsx
+++ src/test/RadarChart.jsx
@@ -1,80 +1,159 @@
-import React, { useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
-import { Radar } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, RadialLinearScale, PointElement, LineElement, Tooltip, Legend } from 'chart.js';
+import React, { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+import { Radar } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	ArcElement,
+	RadialLinearScale,
+	PointElement,
+	LineElement,
+	Tooltip,
+	Legend,
+} from "chart.js";
 
-ChartJS.register(ArcElement, RadialLinearScale, PointElement, LineElement, Tooltip, Legend);
+ChartJS.register(
+	ArcElement,
+	RadialLinearScale,
+	PointElement,
+	LineElement,
+	Tooltip,
+	Legend,
+);
 
-const labels = ['Relations / Love','Health','Business / Career','Finances','Contribution to society','Family and friends','Fun and recreation','Personal growth','divison9' , ' divison10'];
+const labels = [
+	"Relations / Love",
+	"Health",
+	"Business / Career",
+	"Finances",
+	"Contribution to society",
+	"Family and friends",
+	"Fun and recreation",
+	"Personal growth",
+	"Division 9",
+	"Division 10",
+];
+const colors = [
+	"rgba(255, 99, 132, 0.5)",
+	"rgba(54, 162, 235, 0.5)",
+	"rgba(255, 206, 86, 0.5)",
+	"rgba(75, 192, 192, 0.5)",
+	"rgba(153, 102, 255, 0.5)",
+	"rgba(255, 159, 64, 0.5)",
+	"rgba(199, 199, 199, 0.5)",
+	"rgba(255, 99, 132, 0.5)",
+	"rgba(54, 162, 235, 0.5)",
+	"rgba(255, 206, 86, 0.5)",
+];
 
-function ApexChart() {
-  const [series, setSeries] = useState([10, 7, 10, 10, 5, 10, 2, 5, 7, 9]);
-  const [options, setOptions] = useState({
-    chart: { type: 'polarArea' },
-    fill: { opacity: 0.8 },
-    labels: labels,
-    plotOptions: { polarArea: { rings: { strokeWidth: 0 } } },
-    responsive: [{ breakpoint: 480, options: { chart: { width: 200 }, legend: { position: 'bottom' } } }],
-  });
+function ApexChart({ currentValues, desiredValues }) {
+	const calculateOpacity = (value) => {
+		// Map values between 0 and 1 where 10 maps to 1
+		return Math.min(value / 10, 1);
+	};
+	const [options, setOptions] = useState({
+		chart: { type: "polarArea" },
+		fill: {
+			opacity: currentValues.map(calculateOpacity),
+		},
+		labels: labels,
+		plotOptions: { polarArea: { rings: { strokeWidth: 0 } } },
+		responsive: [
+			{
+				breakpoint: 480,
+				options: { chart: { width: 200 }, legend: { position: "bottom" } },
+			},
+		],
+	});
 
-  return (
-    <div style={{ position: 'relative', width: '50%', height: '50%', margin: 'auto' }}>
-      <div id="chart" style={{ position: 'relative', zIndex: 0 }}>
-        
-        <ReactApexChart options={options} series={series} type="polarArea" />
-      </div>
-      <div id="html-dist" className=' z-10'> <RadarChart /></div>
-     
-    </div>
-  );
+	return (
+		<div
+			style={{
+				position: "relative",
+				width: "50%",
+				height: "50%",
+				margin: "auto",
+			}}
+		>
+			<div id="chart" style={{ position: "relative", zIndex: 1 }}>
+				<ReactApexChart
+					options={options}
+					series={currentValues}
+					type="polarArea"
+				/>
+			</div>
+			<div
+				id="html-dist"
+				style={{
+					position: "absolute",
+					top: "12px",
+					left: "10px",
+					right: "0",
+					bottom: "0",
+					zIndex: 2,
+				}}
+			>
+				<RadarChart desiredValues={desiredValues} />
+			</div>
+		</div>
+	);
 }
 
-const RadarChart = () => {
-  const dataPoints = [
-    { current: 10, desired: 5 },{ current: 7, desired: 10 },{ current: 10, desired: 5 },{ current: 10, desired: 10 },{ current: 5, desired: 10 },{ current: 10, desired: 5 },{ current: 2, desired: 10 },{ current: 5, desired: 10 },{ current: 5, desired: 10 },{ current: 5, desired: 10 }];
+const RadarChart = ({ desiredValues }) => {
+	const radarData = {
+		labels: labels,
+		datasets: [
+			{
+				label: "Desired",
+				data: desiredValues,
+				backgroundColor: "rgba(0, 0, 0, 0)",
+				borderColor: "rgba(255, 99, 132, 1)",
+				pointBackgroundColor: "rgba(255, 99, 132, 1)",
+				pointBorderColor: "#fff",
+				pointStyle: "circle",
+				pointRadius: 5,
+			},
+		],
+	};
 
-  const radarData = {
-    labels: labels,
-    datasets: [
-      {
-        label: '',
-        data: dataPoints.map(point => point.desired),
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        pointBackgroundColor: 'rgba(255, 99, 132, 1)',
-        pointBorderColor: '#fff',
-        pointStyle: 'circle',
-        pointRadius: 5,
-      }
-    ],
-  };
+	const radarOptions = {
+		scales: {
+			r: {
+				min: 0,
+				max: 10,
+				ticks: {
+					stepSize: 2,
+					display: false,
+				},
+				pointLabels: { display: false },
+				angleLines: { display: false },
+				grid: { display: true, circular: true },
+			},
+		},
+		elements: {
+			line: { borderWidth: 2 },
+			point: { radius: 3 },
+		},
+		plugins: {
+			legend: { display: false },
+		},
+	};
 
-  const radarOptions = {
-    scales: {
-      r: {
-        min: 0,
-        max: 13,
-        ticks: {
-          stepSize: 2,
-          display: false
-        },
-        pointLabels: { display: false },
-        angleLines: { display: false },
-        grid: { display: false, circular: true },
-      }
-    },
-    elements: {
-      line: { borderWidth: 1 },
-      point: { radius: 2 }
-    }
-  };
-
-  return (
-    <div style={{ position: 'absolute', top: '0', left: '0', bottom:'0', right:'0', width: '100%', height: '100%',  transform: 'rotate(20deg)' }}>
-        <Radar data={radarData} options={radarOptions} />
-     
-    </div>
-  );
+	return (
+		<div
+			style={{
+				position: "absolute",
+				top: "26px",
+				left: "12px",
+				right: "10px",
+				bottom: "0px",
+				width: "100%",
+				height: "100%",
+				transform: "rotate(17deg)",
+			}}
+		>
+			<Radar data={radarData} options={radarOptions} />
+		</div>
+	);
 };
 
 export default ApexChart;
--- src/web/components/answers/CEFinalResult.jsx
+++ src/web/components/answers/CEFinalResult.jsx
@@ -1,91 +1,108 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import PrintIcon from '@mui/icons-material/Print';
-import DownloadIcon from '@mui/icons-material/Download';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import EmailIcon from '@mui/icons-material/Email';
-import { collection, doc, getDocs, getDoc } from 'firebase/firestore';
-import { db, auth } from '../../../../db';
-import 'react-toastify/dist/ReactToastify.css';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PrintIcon from "@mui/icons-material/Print";
+import DownloadIcon from "@mui/icons-material/Download";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
+import { collection, doc, getDocs, getDoc } from "firebase/firestore";
+import { db, auth } from "../../../../db";
+import "react-toastify/dist/ReactToastify.css";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import ApexChart from "../../../test/RadarChart";
 const CEFinalResult = () => {
+	const user = auth.currentUser;
+	const navigate = useNavigate();
+	const [healthAndFitnessQuestions, setHealthAndFitnessQuestions] = useState(
+		[],
+	);
+	const [usersAnswers, setUsersAnswers] = useState([]);
 
-    const user = auth.currentUser;
-    const navigate = useNavigate();
-    const [healthAndFitnessQuestions, setHealthAndFitnessQuestions] = useState([]);
-    const [usersAnswers, setUsersAnswers] = useState([]);
+	const fetchHealthAndFitnessQuestions = async () => {
+		try {
+			const docRef = collection(db, "CEQuestions");
+			const querySnapshot = await getDocs(docRef);
+			const questionsData = querySnapshot.docs.map((doc) => ({
+				id: doc.id,
+				...doc.data(),
+			}));
+			setHealthAndFitnessQuestions(questionsData);
+		} catch (error) {
+			console.error("Error fetching Health & Fitness Questions:", error);
+		}
+	};
 
-    const fetchHealthAndFitnessQuestions = async () => {
-        try {
-            const docRef = collection(db, 'CEQuestions');
-            const querySnapshot = await getDocs(docRef);
-            const questionsData = querySnapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-            setHealthAndFitnessQuestions(questionsData);
-        } catch (error) {
-            console.error('Error fetching Health & Fitness Questions:', error);
-        }
-    };
+	const fetchUsersAnswers = async () => {
+		try {
+			const answersDocRef = doc(db, "CEResponses", user.uid);
+			const answersDoc = await getDoc(answersDocRef);
+			if (answersDoc.exists()) {
+				setUsersAnswers(answersDoc.data().answers);
+			} else {
+				console.error("No such document!");
+			}
+		} catch (error) {
+			console.error("Error fetching user answers:", error);
+		}
+	};
 
-    const fetchUsersAnswers = async () => {
-        try {
-            const answersDocRef = doc(db, 'CEResponses', user.uid);
-            const answersDoc = await getDoc(answersDocRef);
-            if (answersDoc.exists()) {
-                setUsersAnswers(answersDoc.data().answers);
-            } else {
-                console.error('No such document!');
-            }
-        } catch (error) {
-            console.error('Error fetching user answers:', error);
-        }
-    };
+	useEffect(() => {
+		fetchHealthAndFitnessQuestions();
+		if (user) {
+			fetchUsersAnswers();
+		}
+	}, [user]);
 
-    useEffect(() => {
-        fetchHealthAndFitnessQuestions();
-        if (user) {
-            fetchUsersAnswers();
-        }
-    }, [user]);
+	const handlePrint = () => {
+		window.print();
+	};
 
-    const handlePrint = () => {
-        window.print();
-    };
+	const handleSaveAsPDF = () => {
+		const input = document.getElementById("result-content");
+		html2canvas(input).then((canvas) => {
+			const imgData = canvas.toDataURL("image/png");
+			const pdf = new jsPDF("p", "in", "letter");
+			const imgWidth = 8.5;
+			const imgHeight = (canvas.height * imgWidth) / canvas.width;
+			pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+			pdf.save("FinalResult.pdf");
+		});
+	};
+	const currentValues = usersAnswers.map((answer) => answer.current || 0);
+	const desiredValues = usersAnswers.map((answer) => answer.desired || 0);
 
-    const handleSaveAsPDF = () => {
-        const input = document.getElementById('result-content');
-        html2canvas(input)
-            .then((canvas) => {
-                const imgData = canvas.toDataURL('image/png');
-                const pdf = new jsPDF('p', 'in', 'letter'); 
-                const imgWidth = 8.5; 
-                const imgHeight = canvas.height * imgWidth / canvas.width;
-                pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
-                pdf.save('FinalResult.pdf');
-            });
-    };
-
-    return (
-        <>
-            {user ? (
-                <div id="result-content" className="w-full h-auto flex flex-col gap-8 border-t-2 border-gray-300 py-8">
-                    <h1 className="w-full font-bold text-2xl text-center">
-                        Final Result
-                    </h1>
-                    <div className="w-full flex lg:flex-row flex-col justify-evenly items-center gap-2 lg:px-44 px-8">
-                        <button onClick={handlePrint} className="lg:w-auto w-full flex flex-row justify-center items-center gap-2 py-3 px-6 bg-[#2187A2] text-white cursor-pointer rounded-md">
-                            <p className="text-sm lg:text-lg font-bold"> Print </p>
-                            <PrintIcon />
-                        </button>
-                        <button onClick={handleSaveAsPDF} className="lg:w-auto w-full flex flex-row justify-center items-center gap-2 py-3 px-6 bg-[#2187A2] text-white cursor-pointer rounded-md">
-                            <p className="text-sm lg:text-lg font-bold"> Save as PDF </p>
-                            <DownloadIcon />
-                        </button>
-                        {/* <button onClick={() => navigate('')} className="lg:w-auto w-full flex flex-row justify-center items-center gap-2 py-3 px-6 bg-[#2187A2] text-white cursor-pointer rounded-md">
+	return (
+		<>
+			{user ? (
+				<div
+					id="result-content"
+					className="w-full h-auto flex flex-col gap-8 border-t-2 border-gray-300 py-8"
+				>
+					<h1 className="w-full font-bold text-2xl text-center">
+						Final Result
+					</h1>
+					<div>
+						<ApexChart
+							currentValues={currentValues}
+							desiredValues={desiredValues}
+						/>
+					</div>
+					<div className="w-full flex lg:flex-row flex-col justify-evenly items-center gap-2 lg:px-44 px-8">
+						<button
+							onClick={handlePrint}
+							className="lg:w-auto w-full flex flex-row justify-center items-center gap-2 py-3 px-6 bg-[#2187A2] text-white cursor-pointer rounded-md"
+						>
+							<p className="text-sm lg:text-lg font-bold"> Print </p>
+							<PrintIcon />
+						</button>
+						<button
+							onClick={handleSaveAsPDF}
+							className="lg:w-auto w-full flex flex-row justify-center items-center gap-2 py-3 px-6 bg-[#2187A2] text-white cursor-pointer rounded-md"
+						>
+							<p className="text-sm lg:text-lg font-bold"> Save as PDF </p>
+							<DownloadIcon />
+						</button>
+						{/* <button onClick={() => navigate('')} className="lg:w-auto w-full flex flex-row justify-center items-center gap-2 py-3 px-6 bg-[#2187A2] text-white cursor-pointer rounded-md">
                             <p className="text-sm lg:text-lg font-bold"> Share on Facebook </p>
                             <FacebookIcon />
                         </button>
@@ -93,27 +110,39 @@ const CEFinalResult = () => {
                             <p className="text-sm lg:text-lg font-bold"> Email my Wheel </p>
                             <EmailIcon />
                         </button> */}
-                    </div>
-                    {healthAndFitnessQuestions.map((question, index) => (
-                        <div key={index} className="w-full flex flex-col justify-start items-start lg:px-44 px-8">
-                            <h2 className="text-base font-semibold">Question # {" "}{index + 1}{" : "}{question.question}</h2>
-                            <div className='w-full flex flex-row justify-start items-start gap-4' >
-                                <p className="text-base font-semibold" > Answer {" : "} </p>
-                                <p className="text-base text-[#03009C] font-bold">Current Level: {usersAnswers[index]?.current || 'Not Answered'}</p>
-                                <p className="text-base text-[#03009C]  font-bold">Ideal Level: {usersAnswers[index]?.desired || 'Not Answered'}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <div className="w-full h-auto flex flex-col gap-8 border-t-2 border-gray-300 py-8">
-                    <h1 className="w-full font-bold text-2xl text-center">
-                        Please Login First
-                    </h1>
-                </div>
-            )}
-        </>
-    );
+					</div>
+					{healthAndFitnessQuestions.map((question, index) => (
+						<div
+							key={index}
+							className="w-full flex flex-col justify-start items-start lg:px-44 px-8"
+						>
+							<h2 className="text-base font-semibold">
+								Question # {index + 1}
+								{" : "}
+								{question.question}
+							</h2>
+							<div className="w-full flex flex-row justify-start items-start gap-4">
+								<p className="text-base font-semibold"> Answer {" : "} </p>
+								<p className="text-base text-[#03009C] font-bold">
+									Current Level:{" "}
+									{usersAnswers[index]?.current || "Not Answered"}
+								</p>
+								<p className="text-base text-[#03009C]  font-bold">
+									Ideal Level: {usersAnswers[index]?.desired || "Not Answered"}
+								</p>
+							</div>
+						</div>
+					))}
+				</div>
+			) : (
+				<div className="w-full h-auto flex flex-col gap-8 border-t-2 border-gray-300 py-8">
+					<h1 className="w-full font-bold text-2xl text-center">
+						Please Login First
+					</h1>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default CEFinalResult;
